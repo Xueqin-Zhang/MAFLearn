d50c2d43ec72ec48cdce947fcd2c6e59ecaeb22d
Today <2107885241@qq.com>
2026-04-22 22:22:56 +0800

    使用预览版Nuget心累

--- MAFRagDemo/Model.cs
+++ MAFRagDemo/Model.cs
@@ -1,6 +1,14 @@
-﻿namespace MAFRagDemo;
+﻿using Microsoft.Extensions.AI;
+using Microsoft.Extensions.VectorData;
+
+namespace MAFRagDemo;
 
 public class Model
 {
-    
+    [VectorStoreKey] public Guid Key { get; set; }
+    [VectorStoreData] public string? Text { get; set; }
+
+    [VectorStoreData] public string? Link { get; set; }
+
+    [VectorStoreVector(1024)] public Embedding<float>? Embedding { get; set; }
 }
